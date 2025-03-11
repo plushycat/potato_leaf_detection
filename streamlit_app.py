e52efbfafe75e77c7fc6f8cf0835a73bc977fa36
boieee69 <hemanthpyma@gmail.com>
2025-03-11 21:39:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ if not os.path.exists(model_path):
 
 
 def model_prediction(test_image):
-    model=tf.keras.model(model_path)
+    model=tf.keras.models.load_model(model_path)
     image=tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
     input_arr=tf.keras.preprocessing.image.img_to_array(image)
     input_arr=np.array([input_arr])
